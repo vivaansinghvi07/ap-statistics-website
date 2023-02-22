7f0f734cee2b3b68b7e03934ab43dc5ca255c38e
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-21 20:46:30 -0500

    abstracted the graph construction into a function

--- units/unit5/calculators/scripts/simulate.js
+++ units/unit5/calculators/scripts/simulate.js
@@ -20,55 +20,61 @@ function change() {
     .then(data => {
       // copies the data onto the dataset variable
       dataset = data.split(/[\s\n]+/).map(parseFloat);
-
-      // declares bounds and steps for the graph based on the type being analyzed
-      var graphSettings;
-      if (type === "normal") {
-        graphSettings = [-3, 3, 0.2];
-      }
-      else if (type === "skewed") {
-        graphSettings = [-2, 7, 0.2];
-      }
-      else if (type === "uniform") {
-        graphSettings = [0, 1, 0.1];
-      }
-
-      // assigns the settings to variables
-      var [start, end, step] = graphSettings;
-
-      console.log(dataset);
-
-      // creates a layout for the graph based on plotly docs
-      var trace = {
-        x: dataset,
-        type: 'histogram',
-        marker: {
-          color: "rgba(0, 120, 136, 0.7)",
-          line: {
-            color:  "rgba(0, 120, 136, 1)", 
-            width: 1
-          },
-        },
-        xbins: {
-          start: start,
-          end: end, 
-          size: step
-        },
-      };
-
-      // sets a space between bars and title
-      var layout = {
-        bargap: 0.05,
-        yaxis: {
-          title: "Count"
-        }
-      };
-
-      // plots the graph
-      Plotly.newPlot(container1, [trace], layout);
+      
+      // displays the graph
+      displayGraphWithType(type, container1);
+      
     })
     .catch(error => console.error(error));
 
+}
+
+function displayGraphWithType(type, container) {
+    // declares bounds and steps for the graph based on the type being analyzed
+    var graphSettings;
+    if (type === "normal") {
+      graphSettings = [-3, 3, 0.2];
+    }
+    else if (type === "skewed") {
+      graphSettings = [-2, 7, 0.2];
+    }
+    else if (type === "uniform") {
+      graphSettings = [0, 1, 0.1];
+    }
+
+    // assigns the settings to variables
+    var [start, end, step] = graphSettings;
+
+    console.log(dataset);
+
+    // creates a layout for the graph based on plotly docs
+    var trace = {
+      x: dataset,
+      type: 'histogram',
+      marker: {
+        color: "rgba(0, 120, 136, 0.7)",
+        line: {
+          color:  "rgba(0, 120, 136, 1)", 
+          width: 1
+        },
+      },
+      xbins: {
+        start: start,
+        end: end, 
+        size: step
+      },
+    };
+
+    // sets a space between bars and title
+    var layout = {
+      bargap: 0.05,
+      yaxis: {
+        title: "Count"
+      }
+    };
+
+    // plots the graph
+    Plotly.newPlot(container, [trace], layout);
 }
 
 function calculate() {
