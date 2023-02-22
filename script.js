0a7461285b9449bb9f469ab9a760cf26a0c52a7d
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-22 10:33:00 -0500

    moved evalfraction to a gloabal script

---
+++ script.js
@@ -0,0 +1,38 @@
+function evalFraction(str, output) {
+    try {
+        // splits the string based on if there is a slash
+        var num = str.split("/");
+
+        // makes sure someone didnt put a lot of fractions
+        if (num.length > 2) {
+            output.innerHTML = "Don't stack fractions!";
+            return "q";
+        }
+
+        // checks if it was left blank
+        else if (num[0] === "") {
+            output.innerHTML  = "Please don't leave fields blank!";
+            return "q";
+        }
+
+        else {
+            // decimal value if fraction; number if just normal
+            var out = num.length === 2 ? parseFloat(num[0]) / parseFloat(num[1]) : parseFloat(num[0]);
+        
+            // makes sure it is within bounds
+            if (out < 0 || out > 1) {
+                output.innerHTML = "Make sure probabilities are between 0 and 1!";
+                return "q";
+            }
+
+            return out;
+
+        }
+    }
+    
+    // if someone entered letters, throw an error
+    catch (error) {
+        output.innerHTML = "You can only enter numbers!"
+        return "q";
+    }
+}
