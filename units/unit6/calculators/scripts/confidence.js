d729e9fbdf9774252263f833911f68aead11fcc4
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-24 19:47:41 -0500

    prepared template for confidence interval

--- units/unit6/calculators/scripts/confidence.js
+++ units/unit6/calculators/scripts/confidence.js
@@ -1,31 +1,4 @@
-function change () {
-    // gets the type of interval to run
-    var type = document.getElementById("type").value;
-    
-    // gets the settings
-    var propSettings = document.getElementById("prop-settings");
-    var meanSettings = document.getElementById("mean-settings");
-
-    // makes an array of settings
-    var settings = [propSettings, meanSettings];
-
-    // gets the index to skip
-    var index = ["prop", "mean"].findIndex((t) => {
-        if (t === type) {
-            return true;
-        }
-    });
-
-    // goes through the settings array and displaying if needed
-    settings.forEach((element, i) => {
-        if (i === index) {
-            element.removeAttribute("hidden");
-        }
-        else {
-            element.setAttribute("hidden", "hidden");
-        }
-    });
-
-
-
+function change() {
+    changeSettings(["prop", "mean"]);
 }
+
