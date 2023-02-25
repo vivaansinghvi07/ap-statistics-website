158b0a960684cdde9b05cbe7b13c413dbd5fb8b0
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-02-25 13:53:20 -0500

    finished confidence interval for means

--- units/unit6/calculators/scripts/confidence.js
+++ units/unit6/calculators/scripts/confidence.js
@@ -6,6 +6,10 @@ const concludeTitle = "<div style=\"font-size: 28px;\">Conclude:</div>";
 function change() {
     // see script.js for documentation
     changeSettings(["prop", "mean"]);
+    clearIDS(["state", "plan", "do", "conclude", "container"]);
+
+    // hides the border
+    document.getElementById("border").setAttribute("hidden", "hidden");
 }
 
 function calculate() {
@@ -72,7 +76,6 @@ function calculate() {
 
         // gets the critical value to be used in the interval
         let criticalValue = ss.inverseErrorFunction(c) * Math.sqrt(2);
-        console.log(criticalValue);
 
         // calculates values for the interval
         lowerBound = mu - criticalValue * sigma;
@@ -89,11 +92,11 @@ function calculate() {
 
         // prints the plan part
         plan.innerHTML = planTitle + "Declaring Variables: <br>" + "&emsp;\\(\\hat{p} = \\text{[insert context here]} \\quad \\quad " +
-                                                                   "\\mu_{\\hat{p}} = \\hat{p} = " + String(p) + " \\quad \\quad " + 
-                                                                   "\\sigma_{\\hat{p}} = \\sqrt{\\frac{\\hat{p}(1-\\hat{p})}{n}} = \\sqrt{\\frac{" + String(p) + "(1 - " + String(p) + ")}{" + String(n) + "}} = " + sigma.toFixed(3) + "\\)<br><br>" + 
+                                                                   "\\mu_{\\hat{p}} = \\hat{p} = " + p.toFixed(3) + " \\quad \\quad " + 
+                                                                   "\\sigma_{\\hat{p}} = \\sqrt{\\frac{\\hat{p}(1-\\hat{p})}{n}} = \\sqrt{\\frac{" + p.toFixed(3) + "(1 - " + p.toFixed(3) + ")}{" + String(n) + "}} = " + sigma.toFixed(3) + "\\)<br><br>" + 
                                      "Checking Conditions: <br>" + "&emsp;1. We assume the sample was taken randomly, if not explicitly stated<br>" + 
                                                                    "&emsp;2. We assume the population size is larger than " + String(10 * n) + "<br>" + 
-                                                                   "&emsp;3. We need to check the Large Counts Condition: <br>" + "&emsp;&emsp;\\(n\\hat{p} \\ge 10 \\quad \\quad " + String(n) + "\\cdot" + p.toFixed(3) + " \\ge 10 \\quad \\quad " + (n * p).toFixed(3) + "\\ge 10\\)<br>" + 
+                                                                   "&emsp;3. We need to check the Large Counts Condition: <br>" + "&emsp;&emsp;\\(n\\hat{p} \\ge 10 \\quad \\quad \\quad \\quad \\ \\ " + String(n) + "\\cdot" + p.toFixed(3) + " \\ge 10 \\quad \\quad " + (n * p).toFixed(3) + "\\ge 10\\)<br>" + 
                                                                                                                             "&emsp;&emsp;\\(n(1-\\hat{p}) \\ge 10 \\quad \\quad " + String(n) + "\\cdot" + (1-p).toFixed(3) + " \\ge 10 \\quad \\quad " + (n * (1 - p)).toFixed(3) + "\\ge 10\\)<br>" + 
                                                                       "&emsp;&emsp;The condition is " + (largeCounts ? "" : "not ") + "met.";
 
@@ -104,9 +107,9 @@ function calculate() {
         doo.innerHTML = doTitle + "<br>\\(\\text{interval} = \\mu_\\hat{p} \\pm z^* \\cdot \\sigma_\\hat{p}\\)<br>" + 
                                   "<br>\\(\\text{area of tail} = \\frac{1 - c}{2} = \\frac{1 - " + String(c) + "}{2} = " + ((1 - c) / 2).toFixed(3) + "\\)<br>" +
                                   "<br>\\(z^* = P(z < \\text{area of tail}) = P(z < " + ((c - 1) / 2).toFixed(3) + ") = " + (-criticalValue).toFixed(3) + "\\)<br>" + 
-                                  "<br>\\(\\mu_{\\hat{p}} = \\hat{p} = " + String(p) + "\\)<br>" + 
-                                  "<br>\\(\\sigma_{\\hat{p}} = \\sqrt{\\frac{\\hat{p}(1-\\hat{p})}{n}} = \\sqrt{\\frac{" + String(p) + "(1 - " + String(p) + ")}{" + String(n) + "}} = " + sigma.toFixed(3) + "\\)<br>" + 
-                                  "<br>\\(\\text{interval} = " + String(p) + " \\pm " + criticalValue.toFixed(3) + "\\cdot" + sigma.toFixed(3) + " = (" + lowerBound.toFixed(3) + ", " + upperBound.toFixed(3) + ")\\)<br>";
+                                  "<br>\\(\\mu_{\\hat{p}} = \\hat{p} = " + p.toFixed(3) + "\\)<br>" + 
+                                  "<br>\\(\\sigma_{\\hat{p}} = \\sqrt{\\frac{\\hat{p}(1-\\hat{p})}{n}} = \\sqrt{\\frac{" + p.toFixed(3) + "(1 - " + p.toFixed(3) + ")}{" + String(n) + "}} = " + sigma.toFixed(3) + "\\)<br>" + 
+                                  "<br>\\(\\text{interval} = " + p.toFixed(3) + " \\pm " + criticalValue.toFixed(3) + "\\cdot" + sigma.toFixed(3) + " = (" + lowerBound.toFixed(3) + ", " + upperBound.toFixed(3) + ")\\)<br>";
 
         // loads the mathjax
         loadMathJax("do");
@@ -115,9 +118,69 @@ function calculate() {
         conclude.innerHTML = concludeTitle + "We are " + (c * 100) + "% confident that the true proportion of [insert context here] is between " + lowerBound.toFixed(3) + " and " + upperBound.toFixed(3) + ".";
     } 
 
+    else if (type === "mean") {
+
+        // gets the values - m is the sample mean, s is hte sample standard deviation, n is the sample size, c is the confidence level
+        var m = evalNumber(document.getElementById("mean-mu").value, output);
+        var s = evalNumber(document.getElementById("mean-sigma").value, output);
+        var n = evalNumber(document.getElementById("mean-n").value, output);
+        var c = evalNumber(document.getElementById("mean-c").value, output);
+
+        // quits if anything is flagged for a quit
+        [m, s, n, c].forEach((element) => {
+            if (element === "q") {
+                return false;
+            }
+        });
+
+        // converts n to an integer if not already
+        n = parseInt(n);
+
+        // assigns the mean and standard deviation of the interval
+        mu = m;
+        sigma = s / Math.sqrt(n);
+
+        // gets the critical value based on https://gist.github.com/benrasmusen/1261977
+        let criticalValue = tdistr(n-1, (1-c)/2);
+     
+        // gets upper and lower bounds
+        lowerBound = mu - sigma * criticalValue;
+        upperBound = mu + sigma * criticalValue;
+
+        // prints the answer:
+        // prints the state part
+        state.innerHTML = stateTitle + "We will be using a one-sample t-interval to determine a " + String(c*100) + "% confidence interval for the [insert context here]";
+
+        // prints the plan part
+        plan.innerHTML = planTitle + "Declaring Variables: <br>" + "&emsp;\\(\\overline{x} = \\text{[insert context here]} \\quad \\quad \\mu_{\\overline{x}} \\approx \\overline{x} = " + String(mu) + " \\quad \\quad \\sigma_{\\overline{x}} \\approx \\frac{\\s_x}{\\sqrt{n}} = \\frac{" + String(s) + "}{\\sqrt{" + String(n) + "}} = " + sigma.toFixed(3) + "\\) <br><br>" +
+                                     "Checking Conditions: <br>" + "&emsp;1. The sample must be randomly chosen<br>" + 
+                                                                   "&emsp;2. We assume the population size is greater than " + String(n * 10) + " (ten times the sample size)<br>" +
+                                                                   "&emsp;3. The sample size is " + (n >= 30 ? "" : "not ") + "large enough to meet the Central Limit Theorem (must be at least 30)<br>";
+        
+        // loads the MathJax
+        loadMathJax("plan");
+
+        // prints the do part
+        doo.innerHTML = doTitle + "<br>" + "\\(\\text{interval} = \\mu_\\overline{x} \\pm t^* \\cdot \\sigma_\\overline{x}\\)<br>" + 
+                                       "<br>\\(\\text{area of tail} = \\frac{1-c}{2} = \\frac{1-" + String(c) + "}{2} = " + ((1-c)/2).toFixed(3) + "\\)<br>" + 
+                                       "<br>\\(t^* = \\text{invT(area: " + ((1-c)/2).toFixed(3) + ", df: " + String(n-1) + ")} = " + criticalValue.toFixed(3) + "\\)<br>" + 
+                                       "<br>\\(\\mu_\\overline{x} \\approx \\overline{x} = " + String(mu) + "\\)<br>" +
+                                       "<br>\\(\\sigma_\\overline{x} \\approx \\frac{s_x}{n} = \\frac{" + String(s) + "}{\\sqrt{" + String(n) + "}} = " + sigma.toFixed(3) + "\\)<br>" + 
+                                       "<br>\\(\\text{interval} = " + String(mu) + " \\pm " + criticalValue.toFixed(3) + "\\cdot" + sigma.toFixed(3) + " = (" + lowerBound.toFixed(3) + ", " + upperBound.toFixed(3) + ")\\)<br>";
+
+        // loads the MathJax
+        loadMathJax("do");
+
+        // prints the conclude part
+        conclude.innerHTML = concludeTitle + "We are " + String(c*100) + "% confident that the true value of [insert context here] is between " + lowerBound.toFixed(3) + " and " + upperBound.toFixed(3) + ".";
+
+
+    }
+
     // documentation for the graph function in script.js
     plotNormalGraphs(mu, sigma, container, lowerBound, upperBound);
 
     // displays the border at the bottom
     border.removeAttribute("hidden");
 }
+
