837b1b2d7d34d171bcf6152edfbb8fbbbb765170
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-02 11:36:28 -0500

    converted files to php

---
+++ units/unit3/notes/unit3.2.php
@@ -0,0 +1,30 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/templates/notes.php' ?>
+<?php startblock('unit') ?>
+Unit 3 Notes
+<?php endblock() ?>
+<?php startblock('title') ?>
+      Bias
+<?php endblock() ?>
+<?php startblock('notes') ?>
+        <li><strong>Bias&nbsp;</strong>in a study is when it consistently either overestimates or underestimates a value that it is trying to determine
+            <ul>
+                <li>There are several types of bias</li>
+            </ul>
+        </li>
+        <li><strong>Nonresponse bias </strong>is when some of the selected individuals (through sampling) do not response to or refuse to participate in the study
+            <ul>
+                <li>The individuals that do respond may hold more passionate views on the topic, messing with the data</li>
+            </ul>
+        </li>
+        <li><strong>Undercoverage bias</strong> is when the sapling method is unable to reach a certain group of individuals
+            <ul>
+                <li>For example, if you are sampling by calling landline phones, the households without landlines will be left out the survey entirely</li>
+            </ul>
+        </li>
+        <li><strong>Response bias</strong> is when the respondent of a survey answers a question in an unintended manner, for whatever reason
+            <ul>
+                <li>It could be due to confusing wording of the question itself&nbsp;</li>
+                <li>It could also be due to the respondent's own personal feelings (i.e. embarrassment)</li>
+            </ul>
+        </li>
+<?php endblock() ?>
---
+++ units/unit3/notes/unit3.4.php
@@ -0,0 +1,44 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/templates/notes.php' ?>
+<?php startblock('unit') ?>
+Unit 3 Notes
+<?php endblock() ?>
+<?php startblock('title') ?>
+Scope of Inference and Data Ethics
+<?php endblock() ?>
+<?php startblock('notes') ?>
+        <li>If the study includes a random sample, you are able to&nbsp;<strong>generalize&nbsp;</strong>the results to the population</li>
+        <li>If the study is a randomly-assigned experiment, you can determine <strong>causation</strong></li>
+        <li>Since some experiments are morally questionable, they lack realism and therefore lack a clear answer
+            <ul>
+                <li>However, given certain circumstances, you are able to make assumptions about causation from an observational study
+                    <ul>
+                        <li>The association must be strong</li>
+                        <li>The association must be consistent</li>
+                        <li>Larger values of the explanatory variable lead to larger values of the response variable</li>
+                        <li>The cause precedes the effect</li>
+                        <li>The conclusion is plausible</li>
+                    </ul>
+                </li>
+                <li>For example, it is not morally right to conduct an experiment about smoking related to lung cancer
+                    <ul>
+                        <li>However, there exists enough research and data to meet the conditions listed above, so you can reasonably assume smoking leads to lung cancer despite the lack of an experiment</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+        <li>An important thing to consider when making an experiment is&nbsp;<strong>data ethics:</strong>
+            <ul>
+                <li>Studies must be reviewed in advance by an&nbsp;<strong>institutional review board (IRB),&nbsp;</strong>which makes sure the experiment is safe and plausible</li>
+                <li>All individuals participating in the study must have <strong>informed consent</strong>
+                    <ul>
+                        <li>They must be willing to participate AND know what they are participating in</li>
+                    </ul>
+                </li>
+                <li>All personally identifiable data must be kept completely confidential
+                    <ul>
+                        <li>Specific medical records and other personal information are all identifiable (you are able to link them to a person)</li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+<?php endblock() ?>
