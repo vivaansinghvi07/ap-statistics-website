ef8547202afb26925b9e396415d7ab2332aca53f
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-05 10:40:42 -0500

    made index page

--- index.php
+++ index.php
@@ -9,11 +9,15 @@
 <?php endblock() ?>
 
 <?php startblock('description') ?>
-  <div style = "text-align: center" class = "notes">A website built for AP Statistics, with notes and calculators for each topic.</div>
+  <div style = "text-align: center" class = "notes">
+    A website built for AP Statistics, with notes and calculators for each topic.
+    <br>
+    Built by <a href = "https://github.com/vivaansinghvi07" class = "notelink">Vivaan Singhvi</a>. Contact me at <a class = "notelink" href = "mailto: singhvi.vivaan@gmail.com?subject = Stats Website Feedback">singhvi.vivaan@gmail.com</a> if you have any concerns!
+  </div>
 <?php endblock() ?>
 
 <?php startblock('output') ?>
-    <ul class = "notes" style = "font-size: 35px;">
+    <ul class = "notes" style = "font-size: 30px;">
       <li><a class="pagelink" href="/units/unit1/unit1.php">Unit 1: One-Variable Data</a></li>
       <li><a class="pagelink" href="/units/unit2/unit2.php">Unit 2: More with Quantitative Data</a></li>
       <li><a class="pagelink" href="/units/unit3/unit3.php">Unit 3: Experimental Design</a></li>
@@ -22,4 +26,6 @@
       <li><a class="pagelink" href="/units/unit6/unit6.php">Unit 6: Confidence Intervals</a></li>
       <li><a class="pagelink" href="/units/unit7/unit7.php">Unit 7: Hypothesis Testing</a></li>
     </ul>
+
+    <hr class = "bottomborder">
 <?php endblock() ?>
