7ae0df94dd4cc58c1eb25d51e76f0980dc2a0ae7
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-05 10:01:45 -0500

    added credits page

---
+++ credits.php
@@ -0,0 +1,35 @@
+<?php include $_SERVER['DOCUMENT_ROOT'].'/templates/calculators.php' ?>
+
+<?php startblock('header')?>
+  Credits
+<?php endblock() ?>
+
+<?php startblock('title')?>
+  Credits
+<?php endblock() ?>
+
+<?php startblock('description') ?>
+  Here are some resources that made this website possible!
+<?php endblock() ?>
+
+<?php startblock('output') ?>
+    <ul class = "notes">
+        <li>Starnes, D. S., Yates, D. S., &amp; Moore, D. S. (2014). <i>The Practice of Statistics: For the AP® Exam.</i> W.H. Freeman and Company. </li>
+        <li>Libraries: 
+            <ul>
+                <li><a href = "https://www.mathjax.org/#gettingstarted" class = "notelink">MathJax</a></li>
+                <li><a href = "https://plotly.com/" class = "notelink">Plotly</a></li>
+                <li><a href = "https://www.anychart.com/" class = "notelink">AnyChart</a></li>
+                <li><a href = "https://simplestatistics.org/" class = "notelink">SimpleStatistics</a></li>
+                <li><a href = "https://gist.github.com/benrasmusen/1261977" class = "notelink">Ben Rasmusen's Statistics Distributions</a></li>
+            </ul>
+        </li>
+        <li> Frameworks and Tools:
+            <ul>
+                <li><a href = "https://getbootstrap.com/" class = "notelink">Bootstrap</a></li>
+                <li><a href = "https://arshaw.com/phpti/" class = "notelink">PHP Template Inheritance</a></li>
+                <li>Canvas's Text Editor (Has Easy HTML Conversion Built-in)</li>
+            </ul>
+        </li>
+    </ul>
+<?php endblock() ?>
