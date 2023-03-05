5d01614e18afc163ff7bbbf23903b7123e80de67
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-05 11:59:35 -0500

    fixed links

--- templates/unitpages.php
+++ templates/unitpages.php
@@ -15,7 +15,7 @@
     
     <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="../../index.html">AP Statistics Helper</a>
+        <a class="navbar-brand" href="/index.php">AP Statistics Helper</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
