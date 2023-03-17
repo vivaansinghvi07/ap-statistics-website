72e16b71e9d6306705086188f232a71c97233e37
vivaansinghvi07 <s945958@student.knoxschools.org>
2023-03-17 01:52:07 -0400

    showed icon in templates

--- templates/unitpages.php
+++ templates/unitpages.php
@@ -4,6 +4,10 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php startblock('unit') ?><?php endblock() ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
